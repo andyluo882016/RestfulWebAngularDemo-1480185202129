94bfdae4524877935bce0fd3c5ca4aac756d4c42
andyluo882016 <andylu882016@gmail.com>
2016-11-26 11:20:49 -0800

    my first commit

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `<h1>My First Angular App</h1>
+             <h2>My Tesing speed</h2>`
+})
+export class AppComponent { }
